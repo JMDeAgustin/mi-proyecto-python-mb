199cce9f0a2106886f3cff260a141e8131b84d55
Jose María De Agustín Cazorla <jdeagus@emea.corpdir.net>
2025-06-12 16:01:07 +0200

    resolve dataase - env conflicts

--- db/database.py
+++ db/database.py
@@ -2,14 +2,16 @@ import os
 from dotenv import load_dotenv
 from sqlmodel import create_engine, Session
 
+# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+psycopg2://miusuario:supersecret@db:5432/mi_proyecto",
-)
+# Obtener la URL de la base de datos desde las variables de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Engine PostgreSQL
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está configurada en las variables de entorno")
+
+# Crear el motor de conexión a PostgreSQL
 engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 
 def get_session():
